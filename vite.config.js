3cdde2c7ea21bc4aaa6ba39f2724ac0f901145ab
Anton Vakhrushev <anwinged@ya.ru>
2023-08-29 14:40:52 +0300

    Remove unused packages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import sveltePreprocess from 'svelte-preprocess';
 import { VitePWA } from 'vite-plugin-pwa';
 import loadVersion from 'vite-plugin-package-version';
 import manifest from './manifest';
 import path from 'path';
 import svelteSVG from 'vite-plugin-svelte-svg';
-import { visualizer } from 'rollup-plugin-visualizer';
 import rollupPluginsSvelte from 'rollup-plugin-svelte-svg';
-
-import { string } from 'rollup-plugin-string';
 
 export default defineConfig({
   optimizeDeps: {
@@ -18,17 +14,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      // external: ['aws-sdk','aws-sdk/clients/S3'],
-      output: {
-        // globals: {
-        //   'aws-sdk': 'AWS',
-        //   'S3': 'aws-sdk/clients/S3'
-        // },
-      },
-      plugins: [
-        // visualizer({ filename: 'stats.html' }),
-        rollupPluginsSvelte,
-      ],
+      plugins: [rollupPluginsSvelte],
     },
   },
   resolve: {
@@ -42,7 +28,6 @@ export default defineConfig({
     svelteSVG({
       svgoConfig: {}, // See https://github.com/svg/svgo#configuration
     }),
-
     VitePWA({
       manifest: manifest,
       maximumFileSizeToCacheInBytes: 1000 * 1000 * 4,
